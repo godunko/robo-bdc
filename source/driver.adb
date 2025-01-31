010b55d9b7e908c9eeaab8a5695ed3b60d6588ab
Vadim Godunko <vgodunko@gmail.com>
2025-01-31 20:12:26 +0400

    Stub.

---
+++ source/driver.adb
@@ -0,0 +1,10 @@
+--
+--  Copyright (C) 2025, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+procedure Driver is
+begin
+   null;
+end Driver;
